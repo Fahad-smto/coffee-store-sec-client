50a2d0d5dc9816e0dc8a8800c6ea198a058abe46
Fahad-smto <simanto.smto.prog@gmail.com>
2024-08-18 02:50:05 +0600

    add coffee update coffee added

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+ 
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>hello from add coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,39 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home.jsx';
+import UpdateCoffee from './Components/UpdateCoffee.jsx';
+import AddCoffee from './Components/AddCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/updateCoffee',
+        element:<UpdateCoffee></UpdateCoffee>
+      },
+      {
+        path:'/addCoffee',
+        element:<AddCoffee></AddCoffee>
+      }
+
+    ]
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
