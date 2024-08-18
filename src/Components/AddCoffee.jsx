7f656eff972ace0084be0cc5bab3db4873b07474
Fahad-smto <simanto.smto.prog@gmail.com>
2024-08-19 04:25:50 +0600

    form in coffee added

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -1,9 +1,29 @@
- 
+
 
 const AddCoffee = () => {
     return (
         <div>
-            <h1>hello from add coffee</h1>
+            <h1>Add a coffee</h1>
+            <form>
+
+                <div className="flex">
+                    <div>
+                    <h1>Coffee name</h1>
+                     <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered input-accent w-full max-w-xs" />
+                    </div>
+                    <div>
+                    <h1>Coffee name</h1>
+                     <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered input-accent w-full max-w-xs" />
+                    </div>
+                </div>
+
+            </form>
         </div>
     );
 };
